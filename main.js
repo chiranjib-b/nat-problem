0d362ceed1dace72cfc10d1e6fd0d7a11b5dafcd
cbhattacharjee <cbhattacharjee@Pune883.egain.na>
2018-10-07 12:56:10 +0530

    Skeleton Commit

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var path = require('path');
 
+var routes = require('./routes/index');
+app.use('/', routes);
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('port', (8001));
+
+server.listen(app.get('port'), function() {
+    console.log('NAT Test App Started on port ' + app.get('port'));
+});
